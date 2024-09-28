d51b512c6200643aae9333fd19e539c3c860cb21
Michal Hruby <michal.mhr@gmail.com>
2024-09-28 21:36:04 +0100

    updated comments

--- ascii/ascii-apple.c
+++ ascii/ascii-apple.c
@@ -2,82 +2,6 @@
 #include <stdbool.h>
 #include <arm_neon.h>
 
-static uint8_t indexesTable[16] = {
-    1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16};
-
-// gocc: IndexNonASCII(data string) int
-int64_t index_nonascii(const unsigned char *data, const uint64_t length)
-{
-    const uint64_t blockSize = 16; // NEON can process 128 bits (16 bytes) at a time
-
-    uint64_t i = 0;
-
-    if (length >= 8)
-    {
-        uint8x16_t msb_mask = vdupq_n_u8(0x80); // Create a mask for the MSB
-        // uint64x2_t loader = vdupq_n_u64(0x0807060504030201ull);
-        // loader = vsetq_lane_u64(0x100f0e0d0c0b0a09ull, loader, 1);
-        uint8x16_t indexes = vld1q_u8(indexesTable);
-
-        for (; i + blockSize <= length; i += blockSize)
-        {
-            uint8x16_t block = vld1q_u8(data + i);         // Load 16 bytes of data
-            uint8x16_t result = vandq_u8(block, msb_mask); // AND with the mask to isolate MSB
-            // Check if there's any set bit (u32 is faster than u8)
-            if (vmaxvq_u32(result) > 0)
-            {
-                // now a few operations to find the index of the first set bit
-                result = vshrq_n_u8(result, 7); // Shift the MSB to the LSB
-                // multiply the result by the indexes
-                result = vmulq_u8(result, indexes);
-                // we'll use minv, so need to invert zeroes
-                uint8x16_t mmask = vceqzq_u8(result);
-                uint8x16_t multiplied = vorrq_u8(result, mmask);
-
-                return vminvq_u8(multiplied) + i - 1;
-            }
-        }
-
-        if (i + 8 <= length)
-        {
-            // same as above, but for 8 bytes
-            uint8x8_t block = vld1_u8(data + i);
-            uint8x8_t result = vand_u8(block, vget_low_u8(msb_mask));
-            if (vmaxv_u16(result) > 0) // faster than u8
-            {
-                result = vshr_n_u8(result, 7);
-                result = vmul_u8(result, vget_low_u8(indexes));
-                uint8x8_t mmask = vceqz_u8(result);
-                uint8x8_t multiplied = vorr_u8(result, mmask);
-                return vminv_u8(multiplied) + i - 1;
-            }
-            i += 8;
-        }
-    }
-
-    if (i + 4 <= length)
-    {
-        uint32_t data32 = *(uint32_t *)(data + i);
-        data32 &= 0x80808080;
-        if (data32 != 0)
-        {
-            return i + __builtin_ctz(data32) / 8;
-        }
-        i += 4;
-    }
-
-    // Handle the remaining bytes (if any)
-    for (; i < length; i++)
-    {
-        if (data[i] & 0x80)
-        {
-            return i;
-        }
-    }
-
-    return -1;
-}
-
 // gocc: IsASCII(data string) bool
 bool is_ascii(unsigned char *data, uint64_t length)
 {
@@ -163,6 +87,82 @@ bool is_ascii(unsigned char *data, uint64_t length)
     return (data32 & 0x80808080) ? false : true;
 }
 
+static uint8_t indexesTable[16] = {
+    1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16};
+
+// gocc: IndexNonASCII(data string) int
+int64_t index_nonascii(const unsigned char *data, const uint64_t length)
+{
+    const uint64_t blockSize = 16; // NEON can process 128 bits (16 bytes) at a time
+
+    uint64_t i = 0;
+
+    if (length >= 8)
+    {
+        uint8x16_t msb_mask = vdupq_n_u8(0x80); // Create a mask for the MSB
+        // uint64x2_t loader = vdupq_n_u64(0x0807060504030201ull);
+        // loader = vsetq_lane_u64(0x100f0e0d0c0b0a09ull, loader, 1);
+        uint8x16_t indexes = vld1q_u8(indexesTable);
+
+        for (; i + blockSize <= length; i += blockSize)
+        {
+            uint8x16_t block = vld1q_u8(data + i);         // Load 16 bytes of data
+            uint8x16_t result = vandq_u8(block, msb_mask); // AND with the mask to isolate MSB
+            // Check if there's any set bit (u32 is faster than u8)
+            if (vmaxvq_u32(result) > 0)
+            {
+                // now a few operations to find the index of the first set bit
+                result = vshrq_n_u8(result, 7); // Shift the MSB to the LSB
+                // multiply the result by the indexes
+                result = vmulq_u8(result, indexes);
+                // we'll use minv, so need to invert zeroes
+                uint8x16_t mmask = vceqzq_u8(result);
+                uint8x16_t multiplied = vorrq_u8(result, mmask);
+
+                return vminvq_u8(multiplied) + i - 1;
+            }
+        }
+
+        if (i + 8 <= length)
+        {
+            // same as above, but for 8 bytes
+            uint8x8_t block = vld1_u8(data + i);
+            uint8x8_t result = vand_u8(block, vget_low_u8(msb_mask));
+            if (vmaxv_u16(result) > 0) // faster than u8
+            {
+                result = vshr_n_u8(result, 7);
+                result = vmul_u8(result, vget_low_u8(indexes));
+                uint8x8_t mmask = vceqz_u8(result);
+                uint8x8_t multiplied = vorr_u8(result, mmask);
+                return vminv_u8(multiplied) + i - 1;
+            }
+            i += 8;
+        }
+    }
+
+    if (i + 4 <= length)
+    {
+        uint32_t data32 = *(uint32_t *)(data + i);
+        data32 &= 0x80808080;
+        if (data32 != 0)
+        {
+            return i + __builtin_ctz(data32) / 8;
+        }
+        i += 4;
+    }
+
+    // Handle the remaining bytes (if any)
+    for (; i < length; i++)
+    {
+        if (data[i] & 0x80)
+        {
+            return i;
+        }
+    }
+
+    return -1;
+}
+
 static uint8_t uppercasingTable[32] = {
     0,
     32,
@@ -239,6 +239,7 @@ bool equal_fold(unsigned char *a, uint64_t a_len, unsigned char *b, uint64_t b_l
         }
         length %= blockSize;
 
+        // same as above, but with just half the vector register
         if (length >= 8)
         {
             uint8x8_t a_data = vld1_u8(a);
@@ -256,7 +257,7 @@ bool equal_fold(unsigned char *a, uint64_t a_len, unsigned char *b, uint64_t b_l
             // we should shift the data back, but we just need to compare, so we can skip that
             uint8x8_t result = vceq_u8(a_data, b_data);
 
-            // Check if there's any 0 bytes (u32 is faster than u8)
+            // Check if there's any 0 bytes (u16 is faster than u8)
             if (vminv_u16(result) != 0xFFFF)
             {
                 return false;
@@ -327,7 +328,7 @@ bool equal_fold(unsigned char *a, uint64_t a_len, unsigned char *b, uint64_t b_l
     // we should shift the data back, but we just need to compare, so we can skip that
     uint8x8_t result = vceq_u8(a_data, b_data);
 
-    // Check if there's any 0 bytes (u32 is faster than u8)
+    // Check if there's any 0 bytes (u16 is faster than u8)
     if (vminv_u16(result) != 0xFFFF)
     {
         return false;
